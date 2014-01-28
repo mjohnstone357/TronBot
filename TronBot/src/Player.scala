9609bab647a41c2f3ebc591eee31b1a09145ea9b
mjohnstone357 <mjohnstone357@gmail.com>
2014-01-28 13:40:17 +0000

    Initial commit.

---
+++ TronBot/src/Player.scala
@@ -0,0 +1,204 @@
+import scala.io.Source
+import scala.collection.mutable.Map
+
+object Player {
+
+  val GameWidth = 30
+  val GameHeight = 20
+
+
+  def main(args: Array[String]) {
+
+    val gameGrid = new GameGrid(GameWidth, GameHeight)
+
+    while (true) {
+
+      // Impure input stuff
+      val turnInput: List[String] = readOneTurn(Source.stdin)
+
+      val myPlayerNumber = turnInput.head.split(" ")(1).toInt
+      
+      val playerInfosThisTurn: List[PlayerInfo] = PlayerInfo.getPlayerInformation(turnInput)
+      gameGrid.update(playerInfosThisTurn)
+
+
+      // Pure stuff
+      val availableMoves: Set[Move] = gameGrid.getAvailableMoves(myPlayerNumber)
+
+      // Impure output stuff
+
+      if (availableMoves.isEmpty) {
+        debug("Got trapped :(")
+      } else {
+        writeMove(availableMoves.head)
+      }
+    }
+  }
+
+  def readOneTurn(source: Source): List[String] = {
+
+    val linesIterator: Iterator[String] = source.getLines()
+
+    val firstLine: String = linesIterator.next()
+
+    val numbers: Array[String] = firstLine.split(" ")
+    assert(numbers.length == 2)
+
+    val numberOfLinesToFollow = numbers.head.toInt
+
+    val remainingLines: List[String] = (for (i <- 1 to numberOfLinesToFollow) yield linesIterator.next()).toList
+
+    firstLine :: remainingLines
+
+  }
+
+  def writeMove(move: Move): Unit = {
+    println(move.toString)
+  }
+
+  def debug(message: String): Unit = {
+    Console.err.println(message)
+  }
+}
+
+class TurnInfo()
+
+// Try to hide use of Ints
+class GameGrid(width: Int, height: Int) {
+
+  private val EmptySpaceNumber = -1
+  private val playerLocations: Map[Int, Coordinate] = Map[Int, Coordinate]()
+
+  val array = Array.fill(width, height)(EmptySpaceNumber)
+
+  /**
+   * Update the grid using the player information yielded for the latest turn.
+   * @param playerInformation information indicating the location / announcing the death of each player
+   */
+  def update(playerInformation: List[PlayerInfo]) {
+    for(info <- playerInformation) {
+      info match {
+        case PlayerLocation(playerNumber, Coordinate(x,y), _) =>
+          playerLocations(playerNumber) = Coordinate(x,y)
+          array(x)(y) = playerNumber
+        case PlayerDead(playerNumber) => purgePlayer(playerNumber)
+      }
+    }
+  }
+
+  def purgePlayer(playerNumber: Int): Unit = {
+    for(i <- 0 until width; j <- 0 until height) {
+      val cell: Int = array(i)(j)
+      if (cell == playerNumber) {
+        array(i)(j) = EmptySpaceNumber
+      }
+    }
+  }
+
+  def rendered: String = {
+
+    def padInt(x: Int) = {
+      val string: String = x.toString
+      if (string.length == 1) " " + string else string
+    }
+    val paddedCells: Array[Array[String]] = for (row <- array) yield for (cell <- row.reverse) yield padInt(cell)
+    
+    (for (row <- paddedCells) yield row.mkString(" ")).reverse.mkString("\n")
+  }
+
+  def getAvailableMoves(playerNumber: Int): Set[Move] = {
+
+    val playerLocation = playerLocations(playerNumber)
+
+    def cellIsValid(coordinate: Coordinate): Boolean = {
+      val (x, y) = (coordinate.x, coordinate.y)
+      x < width && x >= 0 &&
+      y < height && y >= 0 &&
+      array(x)(y) == EmptySpaceNumber
+    }
+
+    def moveIsAvailable(move: Move): Boolean = {
+      val resultantLocation: Coordinate = playerLocation.applyMove(move)
+      cellIsValid(resultantLocation)
+    }
+
+    for (move <- Move.AllMoves if moveIsAvailable(move)) yield move
+
+  }
+
+}
+
+sealed case class Coordinate(x: Int, y: Int) {
+  def applyMove(move: Move): Coordinate = {
+    move match {
+      case Left() => Coordinate(x - 1, y)
+      case Right() => Coordinate(x + 1, y)
+      case Up() => Coordinate(x, y - 1)
+      case Down() => Coordinate(x, y + 1)
+    }
+  }
+}
+
+abstract sealed class PlayerInfo()
+
+case class PlayerLocation(playerNumber: Int, head: Coordinate, tail: Coordinate) extends PlayerInfo
+
+case class PlayerDead(playerNumber: Int) extends PlayerInfo
+
+object PlayerInfo {
+
+  def getPlayerInformation(turnText: List[String]): List[PlayerInfo] = {
+    val playerInfoLines = turnText.tail
+
+    val firstLine: Array[String] = turnText.head.split(" ")
+    assert(firstLine.size == 2 && playerInfoLines.size == firstLine.head.toInt)
+
+    val zipped: List[(String, Int)] = playerInfoLines.zip(0 until playerInfoLines.size)
+
+    for((playerLine, playerNumber) <- zipped) yield parse(playerLine, playerNumber)
+  }
+
+  def parse(string: String, playerNumber: Int): PlayerInfo = {
+    val values = string.split(" ")
+
+    if (values(0) == "-1") {
+      PlayerDead(playerNumber)
+    } else {
+      val numbers = for(value <- values) yield value.toInt
+      PlayerLocation(
+        playerNumber = playerNumber,
+        head = Coordinate(numbers(2), numbers(3)),
+        tail = Coordinate(numbers(0), numbers(1)))
+    }
+  }
+
+}
+
+object Move {
+  val AllMoves: Set[Move] = Set(Up(), Down(), Left(), Right())
+}
+
+sealed abstract class Move {
+
+  def nextRotation: Move = {
+    this match {
+      case Left() => Up()
+      case Up() => Right()
+      case Right() => Down()
+      case Down() => Left()
+    }
+  }
+}
+
+case class Left() extends Move {
+  override def toString = "LEFT"
+}
+case class Right() extends Move {
+  override def toString = "RIGHT"
+}
+case class Up() extends Move {
+  override def toString = "UP"
+}
+case class Down() extends Move {
+  override def toString = "DOWN"
+}
---
+++ TronBot/src/PlayerTests.scala
@@ -0,0 +1,156 @@
+import org.scalatest._
+import scala.io.Source
+
+class PlayerTests extends FlatSpec with Matchers {
+
+  "The PlayerInfo parse method" should "correctly parse a line containing four input values" in {
+
+    val inputLine = "1 2 3 4"
+    val info = PlayerInfo.parse(inputLine, 7)
+
+    info should be(PlayerLocation(playerNumber = 7, head = Coordinate(3, 4), tail = Coordinate(1, 2)))
+  }
+
+  "The readOneTurn method" should "read from the given source and return a list of lines corresponding to a single turn" in {
+
+    val source = Source.fromString(
+      """2 0
+        |blah
+        |blah
+        |other stuff
+        |more stuff
+      """.stripMargin)
+
+    val turn: List[String] = Player.readOneTurn(source)
+
+    turn should be(List("2 0", "blah", "blah"))
+  }
+
+  "The getPlayerInfos method" should "read a turn's input lines and yield PlayerInfos containing all " +
+    "of the information that can be learned from those lines" in {
+    val input =
+      """2 0
+        |3 5 3 4
+        |-1 -1 -1 -1""".stripMargin
+
+    val inputLines: List[String] = input.lines.toList
+    inputLines.size should be(3)
+
+    val output: List[PlayerInfo] = PlayerInfo.getPlayerInformation(inputLines)
+
+    output should be(List(
+      PlayerLocation(0, head = Coordinate(3, 4), tail = Coordinate(3, 5)),
+      PlayerDead(1)
+    ))
+  }
+
+  "The getAvailableMoves method" should "return no moves when the game grid has only one cell, " +
+    "with the player occupying it" in {
+    val grid = new GameGrid(1, 1)
+    val playerNumber = 0
+
+    grid.update(List(PlayerLocation(playerNumber, Coordinate(0,0), Coordinate(0,0))))
+
+    val availableMoves: Set[Move] = grid.getAvailableMoves(0)
+    availableMoves should be (Set())
+
+  }
+
+  /*
+    0 1 2
+   0    X
+   1
+   2
+  */
+  "The agent" should "consider consider only the directly (not diagonally) adjacent cells, and not the walls, " +
+    "when the game grid is 3x3 and it is in the top left corner" in {
+    val grid = new GameGrid(3, 3)
+    val playerNumber = 0
+
+    grid.update(List(PlayerLocation(playerNumber, Coordinate(2,0), Coordinate(2,0))))
+
+    val availableMoves: Set[Move] = grid.getAvailableMoves(0)
+    availableMoves should be (Set(Left(), Down()))
+
+  }
+
+  /*
+    0 1 2
+   0
+   1  X
+   2
+  */
+  it should "consider moving up, down, left or right when the game grid is 3x3 and the player is in the middle" in {
+    val grid = new GameGrid(3, 3)
+    val playerNumber = 0
+
+    grid.update(List(PlayerLocation(playerNumber, Coordinate(1,1), Coordinate(1,1))))
+
+    val availableMoves: Set[Move] = grid.getAvailableMoves(0)
+    availableMoves should be (Set(Up(), Down(), Left(), Right()))
+
+  }
+
+//  /*
+//    0 1 2
+//   0
+//   1  X
+//   2
+//  */
+//  it should "return all four moves when the game grid is 3x3 and the player is in the middle" in {
+//    val grid = new GameGrid(3, 3)
+//    val playerNumber = 0
+//
+//    grid.update(List(PlayerLocation(playerNumber, Coordinate(1,1), Coordinate(1,1))))
+//
+//    val availableMoves: Set[Move] = grid.getAvailableMoves(0)
+//    availableMoves should be (Set(Up(), Down(), Left(), Right()))
+//
+//  }
+
+  "The game grid" should "render correctly when it is 3x3" in {
+    val grid = new GameGrid(3, 3)
+
+    val expected: String = """-1 -1 -1
+                             |-1 -1 -1
+                             |-1 -1 -1""".stripMargin
+    grid.rendered should be (expected)
+  }
+
+  it should "render correctly when it is 3x3 and includes a single-digit number" in {
+    val grid = new GameGrid(3, 3)
+
+    grid.update(List(
+      PlayerLocation(0, Coordinate(1, 1), Coordinate(1,1)),
+      PlayerLocation(1, Coordinate(2, 0), Coordinate(2,0))))
+
+    val expected: String = """-1 -1  1
+                             |-1  0 -1
+                             |-1 -1 -1""".stripMargin
+    grid.rendered should be (expected)
+  }
+
+  it should "purge a player when requested" in {
+    val grid = new GameGrid(3, 3)
+
+    grid.update(List(PlayerLocation(2, Coordinate(0, 0), Coordinate(0, 0))))
+    grid.update(List(PlayerLocation(2, Coordinate(1, 1), Coordinate(1, 1))))
+    grid.update(List(PlayerLocation(2, Coordinate(2, 2), Coordinate(2, 2))))
+
+    val initialGrid = """ 2 -1 -1
+                        |-1  2 -1
+                        |-1 -1  2""".stripMargin
+    grid.rendered should be (initialGrid)
+
+    grid.purgePlayer(2)
+
+    val resultantGrid = """-1 -1 -1
+                          |-1 -1 -1
+                          |-1 -1 -1""".stripMargin
+
+    grid.rendered should be (resultantGrid)
+  }
+
+}
+
+
